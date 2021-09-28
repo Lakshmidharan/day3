86ba57670bd40346ffdeceef0d3560780b24defa
Lakshmidharan <91528149+Lakshmidharan@users.noreply.github.com>
2021-09-28 14:12:25 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,18 @@
+const resume=[
+    {
+        name: "s.lakshmi dharan",
+        qualification:"bca",
+        DOB:24/04/2001,
+        fathername:"n.selvam",
+        mailid:"lakshmidharan2401@gmail.com",
+        phoneno:8489472925,
+        UGat:"srm trichy arts and science college",
+        HSCat:"svr hr.sec.scl",
+        SSLCat:"st.mary's hr sec scl",
+        areaofinterest:"web developing",
+        languagesknown:"Tamil,english",
+        computerproficiency:"c,c++"
+    }
+
+]
+console.log(resume)
